850972df2071bbbe9da3189af5b2f36acfbe5a9c
Keith Noguchi <hack@noguchi.us>
2022-12-27 14:53:30 -0800

    main: Add health_check with test

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 use std::io;
 
 #[tokio::main]
@@ -7,6 +7,7 @@ async fn main() -> io::Result<()> {
         App::new()
             .route("/", web::get().to(greet))
             .route("/{name}", web::get().to(greet))
+            .route("/health-check", web::get().to(health_check))
     })
     .bind("127.0.0.1:8000")?
     .run()
@@ -16,4 +17,19 @@ async fn main() -> io::Result<()> {
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
     format!("Hello {name}")
+}
+
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::health_check;
+
+    #[tokio::test]
+    async fn health_check_success() {
+        let resp = health_check().await;
+        assert!(resp.status().is_success());
+    }
 }
