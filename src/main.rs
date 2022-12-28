4621e04bc80a0acdda148b297629aba02dcd021c
Keith Noguchi <hack@noguchi.us>
2022-12-27 18:29:48 -0800

    startup: Connect to postgres

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,19 @@
 use std::io;
 use std::net::TcpListener;
 
+use sqlx::{Connection, PgConnection};
+
 use wpr::config;
 use wpr::startup;
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
     let config = config::load().expect("Config load error");
-
+    let connection = PgConnection::connect(&config.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres");
     let address = format!("127.0.0.1:{}", config.application_port);
     let listener = TcpListener::bind(address)?;
 
-    startup::run(listener)?.await
+    startup::run(listener, connection)?.await
 }
--- src/startup.rs
+++ src/startup.rs
@@ -3,15 +3,18 @@ use std::net::TcpListener;
 
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
+use sqlx::PgConnection;
 
 use crate::routes::health_check::health_check;
 use crate::routes::subscriptions::subscribe;
 
-pub fn run(listener: TcpListener) -> io::Result<Server> {
-    let server = HttpServer::new(|| {
+pub fn run(listener: TcpListener, connection: PgConnection) -> io::Result<Server> {
+    let connection = web::Data::new(connection);
+    let server = HttpServer::new(move || {
         App::new()
             .route("/subscriptions", web::post().to(subscribe))
             .route("/health_check", web::get().to(health_check))
+            .app_data(connection.clone())
     })
     .listen(listener)?
     .run();
